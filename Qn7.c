eb8b7ed1af1e3dcfa61e166a31c80b2fa55acec4
NIKIL S <107380156+Nikil2580@users.noreply.github.com>
2023-03-02 09:48:12 +0530

    Update Qn7.c

--- Qn7.c
+++ Qn7.c
@@ -3,6 +3,6 @@
 
 int main()
 {
-    printf("%d\n", rand() % 1000);
-    return 0;
+    printf("%d\n", rand() % 1000);   // Ans: 41 
+    return 0;                        // Always the first random no will 41 because it is a pseudo random number
 }
