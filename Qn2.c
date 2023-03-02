dccc5544f881170541cd51a605afee7fe926719b
NIKIL S <107380156+Nikil2580@users.noreply.github.com>
2023-03-02 09:39:45 +0530

    Update Qn2.c

--- Qn2.c
+++ Qn2.c
@@ -2,7 +2,7 @@
 
 void main()
 {
-    int x = 5, p = 10;
-    printf("%*d", x, p); // Ans: 10
-    getchar();
+    int x = 20, p = 10;
+    printf("%*d", x, p); // Ans:                  10
+    getchar();           //     18 spaces will be printed followed by 10 since * denotes the width of the output string
 }
