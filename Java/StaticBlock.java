caec42cc2828ed4f846521e71c9148226c1e9656
NIKIL S <107380156+Nikil2580@users.noreply.github.com>
2023-03-11 21:48:03 +0530

    Update StaticBlock.java

--- Java/StaticBlock.java
+++ Java/StaticBlock.java
@@ -7,3 +7,6 @@ public class StaticBlock {
         System.out.println("Covai.Co");
     }
 }
+
+// JVM loads the main class and first executes the static block and then executes the main method
+// Static block can be used to open the database or file before the main method executes
